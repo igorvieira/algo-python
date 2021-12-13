67336cadefe76ff4b2d1bb315740b270d2b1e638
Igor Vieira <igor.p.r.vieira@gmail.com>
2021-12-13 20:47:04 -0300

    lesson 4

---
+++ lesson4.py
@@ -0,0 +1,19 @@
+"""
+Created on Sat Dec 13 2021
+
+@author: Vieira
+Percentual de imposto cobrado em CLT e PJ
+"""
+
+
+num = input("Digite o valor do seus ganhos mensais: ")
+
+result1 = float(num) * 0.275
+result2 = float(num) * 0.175
+
+
+print("Seu salário CLT: ", result1)
+print("Seu salário PJ: ", result2) 
+
+
+
