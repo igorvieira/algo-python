813dab1c5ca61094882b2a080dd29a8b9ce284e4
Igor Vieira <igor.p.r.vieira@gmail.com>
2021-12-13 21:02:11 -0300

    lesson 5

---
+++ lesson5.py
@@ -0,0 +1,20 @@
+"""
+Created on Sat Dec 13 2021
+
+@author: Vieira
+Média entre as notas de uma matéria de matemática no ano
+"""
+
+
+num1 = input("Digite um primeiro valor: ")
+num2 = input("Digite um segundo valor: ")
+num3 = input("Digite um terceiro valor: ")
+num4 = input("Digite um quarto valor: ")
+
+
+result = (float(num1) + float(num2) + float(num3) + float(num4))/4
+
+print("Resultado da média: ", result)
+
+
+
